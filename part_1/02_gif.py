848c5c2dde7acffc0d33390e269c0ae812894918
Rob Stenson <rob.stenson@gmail.com>
2021-09-29 09:41:30 -0700

    add gif example

---
+++ part_1/02_gif.py
@@ -0,0 +1,16 @@
+from coldtype import *
+
+@animation((540, 540/2), timeline=20, bg=rgb(0, 0.5, 1), render_bg=1)
+def giftest(f):
+    
+    return PS([
+        (P(f.a.r)
+            .f(hsl(f.e("eeio", 1, rng=(0.6, 0.8)), 0.8, 0.6))),
+        (StSt("COLDTYPE",
+            Font.ColdtypeObviously(),
+            font_size=f.e("eeio", 1, rng=(90, 250)),
+            wdth=f.e("eeio", 1, rng=(1, 0)))
+            .align(f.a.r)
+            .f(1))])
+
+release = giftest.export(fmt="gif")
